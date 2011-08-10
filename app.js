33890c497974653d68ef60a675fb126201e606d9
George Moomau <tallsbyxiv@gmail.com>
2011-08-10 15:58:49 -0400

    moved spotify calls into a separate module

--- app.js
+++ app.js
@@ -10,38 +10,36 @@ var io = require('socket.io').listen(app);
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+    app.use(express.errorHandler()); 
 });
 
 
 // Routes
 var numUsers = 0;
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'sup son',
-    actives: ++numUsers
-  });
+    res.render('index', {
+        title: 'sup son',
+        actives: ++numUsers
+    });
 });
 
 
 // Socket.io Server stuff
 var curQ = [];
-var httpReg = /http:\/\/open\.spotify\.com\/track\/(.*)/;
-var spotReg = /spotify:track:(.*)/;
-var uriLookup = require('http').createClient(80, 'ws.spotify.com');
+var spotify = require('./spotApi.js');
 
 io.sockets.on('connection', function(socket){
     console.log("new client connected");
@@ -53,49 +51,20 @@ io.sockets.on('connection', function(socket){
     });
 
     socket.on('queueUp', function(song){
-        // get track info from spotify
-        // http://developer.spotify.com/en/metadata-api/lookup/track/
-
-        // validate uri
-        var match = httpReg.exec(song);
-        if(match){
-            song = "spotify:track:" + match[1];
-        }
-        match = spotReg.exec(song);
-        if(match){
-            // get info from spotify
-            var songInfoRaw = "";
-            var reqUrl = "/lookup/1/.json?uri="+match[0];
-            var spotReq = uriLookup.request("GET", reqUrl.toString("utf8"),
-                {'host': 'ws.spotify.com'});
-            spotReq.end();
-            spotReq.on('response', function(response){
-                console.log('STATUS: ' + response.statusCode);
-                response.setEncoding('utf8');
-                response.on('data', function(chunk){
-                    songInfoRaw += chunk;
-                });
-                response.on('end', function(){
-                    // add track to queue
-                    curQ.push(songInfoRaw);
-                    io.sockets.emit('songForList', songInfoRaw);
-                });
-            });
-        }
-        else{
-            console.log("invalid uri " + song);
-        }
-        
+        // if song is valid, get info
+        spotify.apiLookup(song, function(songInfo){
+            curQ.push(songInfo);
+            io.sockets.emit('songForList', songInfo);
+            console.log("curQ is: " + curQ);
+        });
     });
 
     socket.on('startPlayback', function(client){
-        if(curTimeout){
+        if(curTimeout != null){
             clearTimeout(curTimeout);
         }
         playNextSong();
     });
-
-
 });
 
 // song starting function
@@ -111,6 +80,7 @@ function playNextSong(){
     }
 }
 
+require("./test.js");
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
