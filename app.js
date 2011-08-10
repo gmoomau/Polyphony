fcaac6af3b43d9591b4537895f7e73a70712f435
George Moomau <tallsbyxiv@gmail.com>
2011-08-10 16:09:20 -0400

    allowed heroku to dictate the port to listen on

--- app.js
+++ app.js
@@ -81,5 +81,6 @@ function playNextSong(){
 }
 
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
