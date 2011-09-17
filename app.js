6226333e27d98b97adc213fea9675448327f344a
root <root@vps-1067276-5253.manage.myhosting.com>
2011-09-16 20:59:10 -0400

    initial commit of code from home pc

---
+++ app.js
@@ -0,0 +1,121 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var app = express.createServer();
+var io = require('socket.io').listen(app);
+
+// Configuration
+
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler()); 
+});
+
+
+// Routes
+var numUsers = 0;
+app.get('/', function(req, res){
+    res.render('index', {
+        title: 'sup son',
+         actives: ++numUsers
+    });
+    //    res.writeHead(200, {'Content-Type': 'text/html'});
+});
+
+app.get('/:room', function(req, res){
+  res.render('room', {
+    room: req.params.room,
+    actives: 924
+  });
+
+});
+
+//s.emit("<script type='text/javascript'> alert(foo); </script>");
+
+// Socket.io Server stuff
+var curQ = [];
+var spotify = require('./spotApi.js');
+var votes = [0,0,0];
+
+io.sockets.on('connection', function(socket){
+    console.log("new client connected");
+
+    // send current queue to client
+    curQ.forEach(function(item){
+        console.log(item);
+        socket.emit('songForList', item);
+    });
+
+    socket.on('queueUp', function(song){
+        // if song is valid, get info
+        spotify.apiLookup(song, function(songInfo){
+            curQ.push(songInfo);
+            io.sockets.emit('songForList', songInfo);
+            console.log("curQ is: " + curQ);
+        });
+    });
+
+    socket.on('startPlayback', function(client){
+        if(curTimeout != null){
+            clearTimeout(curTimeout);
+        }
+        playNextSong();
+    });
+
+    socket.on('vote', function(vote) {
+	console.log(vote + " vote");
+        if(vote == 'good') {
+	    votes[0] = votes[0] + 1;
+        }
+        else if(vote == 'neutral') {
+	    votes[1] = votes[1] + 1;
+        }
+        else if (vote == 'bad') {
+	    votes[2] = votes[2] + 1;
+        }
+        console.log(votes);
+        io.sockets.emit('votes', {'good':votes[0],'neutral':votes[1],'bad':votes[2]});
+    });
+
+    io.sockets.emit('votes', {'good':votes[0],'neutral':votes[1],'bad':votes[2]});
+
+});
+
+
+
+
+// song starting function
+var curTimeout = null;
+function playNextSong(){
+    if(curQ.length > 0){    
+        var songInfoRaw = curQ.shift();
+        var songInfo = JSON.parse(songInfoRaw);
+        votes[0] = 0;
+        votes[1] = 0;
+        votes[2] = 0;
+        io.sockets.emit('changeSong', songInfo.track.href);
+        io.sockets.emit('votes', {'good':votes[0],'neutral':votes[1],'bad':votes[2]});
+        curTimeout = setTimeout(function(){
+            playNextSong();
+        }, songInfo.track.length*1000);
+    }
+}
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ spotApi.js
@@ -0,0 +1,44 @@
+// get track info from spotify
+// http://developer.spotify.com/en/metadata-api/lookup/track/
+
+
+var httpReg = /http:\/\/open\.spotify\.com\/track\/(.*)/;
+var spotReg = /spotify:track:(.*)/;
+var uriLookup = require('http').createClient(80, 'ws.spotify.com');
+
+
+this.apiLookup = function(uri, cb){
+    var match = httpReg.exec(uri);
+    if(match){
+        uri = "spotify:track:" + match[1];
+    }
+    match = spotReg.exec(uri);
+    if(match){
+        // get info from spotify
+        var uriInfoRaw = "";
+        var reqUrl = "/lookup/1/.json?uri="+match[0];
+        var spotReq = uriLookup.request("GET", reqUrl.toString("utf8"),
+                {'host': 'ws.spotify.com'});
+        spotReq.end();
+        spotReq.on('response', function(response){
+            console.log('STATUS: ' + response.statusCode);
+            response.setEncoding('utf8');
+            response.on('data', function(chunk){
+                uriInfoRaw += chunk;
+            });
+            response.on('end', function(){
+                cb(uriInfoRaw);
+            });
+        });
+    }
+    else{
+//        return null;
+        console.log("invalid uri");
+    }
+
+}
+
+module.exports = this;
+
+
+
