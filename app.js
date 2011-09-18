23bb72b7f288b4bcf887bd4f82a297eb42feaeca
George Moomau <tallsbyxiv@gmail.com>
2011-09-18 14:00:41 -0400

    Chat names and messages are now sanitized

--- app.js
+++ app.js
@@ -6,33 +6,35 @@
 var express = require('express');
 var app = express.createServer();
 var io = require('socket.io').listen(app);
+var sanitize = require('validator').sanitize,
+    check = require('validator').check;
 
 // Configuration
 
 app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-    app.use(express.errorHandler()); 
+  app.use(express.errorHandler()); 
 });
 
 
 // Routes
 app.get('/', function(req, res){
-    res.render('index', {
-        title: 'sup son',
-         actives: 0
-    });
+  res.render('index', {
+    title: 'sup son',
+    actives: 0
+  });
 });
 
 app.get('/:room', function(req, res){
@@ -52,124 +54,132 @@ var users = {};          // keeps track of user count per room
 var clients = [];        // keeps track of info per socket
 
 io.sockets.on('connection', function(socket){
-    console.log("new client connected");
+  console.log("new client connected");
 
-    clients[socket] = {vote : 'neutral'};
+  clients[socket] = {vote : 'neutral'};
 
-    // send current queue to client
-    curQ.forEach(function(item){
-        console.log(item);
-        socket.emit('songForList', item);
+  // send current queue to client
+  curQ.forEach(function(item){
+    console.log(item);
+    socket.emit('songForList', item);
+  });
+
+  socket.on('queueUp', function(song){
+    // if song is valid, get info
+    spotify.apiLookup(song, function(songInfo){
+      curQ.push(songInfo);
+      socket.get('room', function(err,room) {
+        io.sockets.in(room).emit('songForList', songInfo);
+        console.log("\n******curQ is: " + curQ);
+      });
     });
+  });
 
-    socket.on('queueUp', function(song){
-        // if song is valid, get info
-        spotify.apiLookup(song, function(songInfo){
-            curQ.push(songInfo);
-            socket.get('room', function(err,room) {
-                 io.sockets.in(room).emit('songForList', songInfo);
-                 console.log("\n******curQ is: " + curQ);
-            });
-        });
+  socket.on('startPlayback', function(client){
+    if(curTimeout != null){
+      clearTimeout(curTimeout);
+    }
+    socket.get('room', function(err,room) {
+      playNextSong(room);
     });
+  });
 
-    socket.on('startPlayback', function(client){
-        if(curTimeout != null){
-            clearTimeout(curTimeout);
-        }
-        socket.get('room', function(err,room) {
-          playNextSong(room);
-	});
-    });
+  // User changed vote
+  socket.on('vote', function(vote) {
+    var prev = clients[socket].vote;
 
-    // User changed vote
-    socket.on('vote', function(vote) {
-	var prev = clients[socket].vote;
-        
-	if (prev != vote) {  // ignore if they vote for the same thing
-            socket.get('room', function(err,room) { // get room from socket
-               console.log(votes[room][prev]);
-               votes[room][prev] -= 1;
-               votes[room][vote] += 1;
-               clients[socket].vote = vote;
-               io.sockets.in(room).emit('votes', votes[room]);
-		});
-        }
-    });
-
-    // when user disconnects, we have to decrement users and votes
-    socket.on('disconnect', function() {
-	votes[clients[socket].vote] -= 1;
-        socket.get('room', function(err,room) {
-          if(room != null && room in users) { 
-               users[room]--;
-               votes[room][clients[socket].vote]--;
-               io.sockets.in(room).emit('votes', votes[room]);
-               io.sockets.in(room).emit('users', users[room]);
-               // maybe get rid of room from the users/votes hashes if no one's in them?
-          }
-        });
-    });
-
-    socket.on('chat name', function(name) {
-	    clients[socket].name = name;
-	});
-
-    socket.on('chat message', function(msg) {
-        socket.get('room', function(err,room) {
-		io.sockets.in(room).emit('chat', clients[socket].name, msg+'<p>');
-	    });
-	});
-
-    // join a user to a given room
-    socket.on('join room', function(room) {
-        if (room in users){  // if the room already exists, increment counts
-          users[room]++;
-          votes[room]['neutral']++;
-        }
-       else {   // otherwise we have to set the counts
-            users[room] = 1;
-            votes[room] = {'good' : 0, 'neutral' : 1, 'bad' : 0};
-        }
-
-	console.log('JOINED '+room+' VOTES:'+votes[room]);
-        socket.set('room', room);    // set the room var so we can join in later
-	socket.join(room);           // actually join the room
-
-        // update votes/users info for everyone in the room
+    if (prev != vote) {  // ignore if they vote for the same thing
+      socket.get('room', function(err,room) { // get room from socket
+        console.log(votes[room][prev]);
+        votes[room][prev] -= 1;
+        votes[room][vote] += 1;
+        clients[socket].vote = vote;
         io.sockets.in(room).emit('votes', votes[room]);
-	io.sockets.in(room).emit('users', users[room]);
-        
+      });
+    }
+  });
+
+  // when user disconnects, we have to decrement users and votes
+  socket.on('disconnect', function() {
+    votes[clients[socket].vote] -= 1;
+    socket.get('room', function(err,room) {
+      if(room != null && room in users) { 
+        users[room]--;
+        votes[room][clients[socket].vote]--;
+        io.sockets.in(room).emit('votes', votes[room]);
+        io.sockets.in(room).emit('users', users[room]);
+        // maybe get rid of room from the users/votes hashes if no one's in them?
+      }
     });
- 
+  });
+
+  socket.on('chat name', function(name) {
+    try{
+      if(check(name).isAlphanumeric()){
+        clients[socket].name = name;
+      }
+    }
+    catch (e){
+      clients[socket].name = "hax0r";
+    }
+  });
+
+  socket.on('chat message', function(msg) {
+    socket.get('room', function(err,room) {
+      var cleaned = sanitize(msg).xss();
+      io.sockets.in(room).emit('chat', clients[socket].name, cleaned);
+    });
+  });
+
+  // join a user to a given room
+  socket.on('join room', function(room) {
+    if (room in users){  // if the room already exists, increment counts
+      users[room]++;
+      votes[room]['neutral']++;
+    }
+    else {   // otherwise we have to set the counts
+      users[room] = 1;
+      votes[room] = {'good' : 0, 'neutral' : 1, 'bad' : 0};
+    }
+
+  console.log('JOINED '+room+' VOTES:'+votes[room]);
+  socket.set('room', room);    // set the room var so we can join in later
+  socket.join(room);           // actually join the room
+
+  // update votes/users info for everyone in the room
+  io.sockets.in(room).emit('votes', votes[room]);
+  io.sockets.in(room).emit('users', users[room]);
+
+  });
+
 });
 
 // song starting function
 var curTimeout = null;
 function playNextSong(room){
-    if(curQ.length > 0){    
-        var songInfoRaw = curQ.shift();
-        var songInfo = JSON.parse(songInfoRaw);
+  if(curQ.length > 0){    
+    var songInfoRaw = curQ.shift();
+    var songInfo = JSON.parse(songInfoRaw);
 
-	io.sockets.in(room).emit('changeSong', songInfo.track.href);
-             
-	// Set votes in the room to be all neutral
-	votes[room]['good'] = 0;
-	votes[room]['neutral'] = users[room];
-	votes[room]['bad'] = 0;
+    io.sockets.in(room).emit('changeSong', songInfo.track.href);
 
-        // Reset client votes from room to be neutral
-        var room_clients = io.sockets.clients(room);
-        room_clients.forEach(function(room_client) {
-		clients[room_client].vote = 'neutral';           
-	});
+    // Set votes in the room to be all neutral
+    votes[room]['good'] = 0;
+    votes[room]['neutral'] = users[room];
+    votes[room]['bad'] = 0;
 
-        io.sockets.in(room).emit('votes', votes[room]);
+    // Reset client votes from room to be neutral
+    var room_clients = io.sockets.clients(room);
+    room_clients.forEach(function(room_client) {
+      clients[room_client].vote = 'neutral';           
+    });
 
-        curTimeout = setTimeout(function(){
-            playNextSong(room);
-        }, songInfo.track.length*1000);
-    }
+    io.sockets.in(room).emit('votes', votes[room]);
+
+    curTimeout = setTimeout(function(){
+      playNextSong(room);
+    }, songInfo.track.length*1000);
+  }
 }
 
 var port = process.env.PORT || 3000;
