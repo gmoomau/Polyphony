c0cef0491a3dd3b6fad71c2656a97eb5a08c61e6
Patrick McQuighan <pmcquighan@gmail.com>
2011-09-28 22:22:33 -0400

    added call to the callback for each thing in redis.js

--- redis.js
+++ redis.js
@@ -27,13 +27,14 @@ this.isNameTaken = function (name, roomName, callback) {
     // check room to see if the given name is taken
     redisClient.sismember('room:'+roomName+'user.names',name, 
       function(err,reply) {
-         return reply;
+          callback(reply);
     });
 }
 
 // if user name is already taken in the room, then we add digits to it to 
 // make it unique
 // if roomName == null, then we ignore that check entirely
+// return false if username was taken
 this.setUserName = function(userId, roomName, newName, callback) {
     console.log('\n************ changing user name');
     var taken = false;
@@ -60,12 +61,13 @@ this.setUserName = function(userId, roomName, newName, callback) {
 	       });
            }
         }
+        // if roomName != null it won't get here until after going through the while, right?
         // Set new name for user
-        redisClient.set('user:'+userId+':name', newName, function(err,res) {});
+        redisClient.set('user:'+userId+':name', newName, function(err,res) {
+           callback(taken);
+         });
 
       });
-  // return false if username was taken
-  return taken;
 }
 
 
@@ -73,7 +75,7 @@ this.getUserName = function(userId, callback) {
     // return the user's name
     redisClient.get('user:'+userId+':name',
       function(err, name) {
-         return name; 
+          callback(name);
       }
     );
 
@@ -83,7 +85,7 @@ this.getUserRoom = function(userId, callback) {
     // return the name of the room that the user is in
     redisClient.get('user:'+userId+':room', 
       function(err,roomName) {
-         return roomName;
+          callback(name);
       });
 }
 
@@ -91,21 +93,21 @@ this.getRoomNextSongs = function(roomName, callback) {
     // get the songs from the next up queue 
     redisClient.get('room:'+roomName+':next.songs',
       function(err, nextSongs) {
-         return nextSongs;
+           callback(nextSongs);
       });
 }
 
 this.getRoomCurSong = function(roomName, callback) {
     redisClient.get('room:'+roomName+':cur.song',
       function(err, curSong) {
-         return curSong;
+          callback(nextSongs);
       });
 }
 
 this.getRoomPrevSongs = function(roomName, callback) {
     redisClient.get('room:'+roomName+':prev.songs',
       function(err, prevSongs) {
-         return prevSongs;
+          callback(prevSongs);
       });
 }
 
@@ -119,7 +121,7 @@ this.removeVote = function(voteId, callback) {
 
 this.doesRoomExist = function(roomName, callback) {
     // return true if the room already exists
-    return true;
+    callback(true);
 }
 
 this.addRoom = function(roomName, callback) {
@@ -127,21 +129,24 @@ this.addRoom = function(roomName, callback) {
     // be sure to use SETNX for this stuff to avoid race condition
     // when two users try to add a room at the same time
     // return false if room already exists. o/w return true
+    callback(true);
 }
 
 this.addUserToRoom = function(userId, roomName, callback) {
     // set user's room id
     // add user's id and user's name to the room
+    callback(true);
 }
 
 this.removeUserFromRoom = function(userId, roomName, callback) {
     // remove roomName from user's room id
     // remove username and user id from room
+    callback(true);
 }
 
 this.getUsersInRoom = function(roomName, callback) {
     // returns a list of user id/names
-    return [];
+    callback([]);
 }
 
 
@@ -151,7 +156,7 @@ this.addSong = function(songObj, callback) {
     // Get a new id for the song and set it
     // Convert object to a string
     // Add spotifyObject, votes and vote.total to the database
-    return 1;
+    callback(1);
 }
 
 this.addSongToRoom = function(songId, roomName, callback) {
@@ -161,21 +166,21 @@ this.addSongToRoom = function(songId, roomName, callback) {
 this.getNewSongId = function(, callback) {
     redisClient.incr('next.user.id', function(err,newid) {
        if(!err) { 
-         return newid;
+           callback(newid);
        }
     });   
 }
 
 this.getNewVoteId = function(, callback) {
     // returns a new vote Id which can be used
-    return 1;
+    callback(1);
 }
 
 this.getVoteId = function(userId, songId, callback) {
     // return the id of the vote associated with this user and song
     // get it by intersecting the user's vote list and song's vote list
     // return a new id if the vote is not found
-    return 1;
+    callback(1);
 }
 
 this.updateVote = function(songId, voteId, newValue, callback) {
@@ -183,19 +188,19 @@ this.updateVote = function(songId, voteId, newValue, callback) {
     // updates a votes value, and add newValue to the song's vote total
     // set value of song in sorted set 
     // returns the new score of the song / number of users by calling getSongAvg
-    return 75;
+    callback(75);
 }
 
 this.getSongAvg = function(songId, callback) {
     // compute the song's new score 
     // return the song's new voter average
-    return 75;
+    callback(75);
 }
 
 this.getTopSongs = function(roomName, numSongs, callback) {
     // Get the top numSongs number of song objs from the room's next queue
     // and return it
-    return [];
+    callback([]);
 }
 
 module.exports = this;
