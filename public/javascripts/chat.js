13448353653ead23052992f6dcbd06868f7d9195
Patrick McQuighan <pmcquighan@gmail.com>
2011-10-04 21:45:11 -0400

    got rid of alert from previous debugging

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -34,7 +34,6 @@ function initChat(socket) {
     var newName = $("#chatNameSet").val();
     $("#chatNameSet").val('');
     socket.emit('chat name',newName);
-    alert('setting name to ' + newName);
   });
 
   $("#chatMessage").keypress(function(e) {
